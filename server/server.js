1ecc5a1a2a4d1e5326cc18fced4e23b958a33583
Kiki <horn_kristof@hotmail.com>
2018-06-18 21:35:20 +0200

    ADD POST / users route and token method

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-require('./config/config.js')
+require('./config/config.js');
 
-const _=require('lodash');
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -97,6 +97,19 @@ app.delete('/todos/:id', (req,res)=>{
     });
   });
 
+  app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+
+    user.save().then(() => {
+      return user.generateAuthToken();
+    }).then((token)=>{
+      res.header('x-auth', token).send(user);
+    }).catch((e) => {
+      res.status(400).send(e);
+    })
+  });
 app.listen(port, () => {
   console.log(`Started up on ${port}`);
 });
