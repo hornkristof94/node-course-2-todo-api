72d30e2f4377ffb37bfe12432e8cf77f2dbc423e
Kiki <horn_kristof@hotmail.com>
2018-06-18 17:51:52 +0200

    update mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
+
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://hornkristof94:123456aA@ds263460.mlab.com:63460/kiki' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
