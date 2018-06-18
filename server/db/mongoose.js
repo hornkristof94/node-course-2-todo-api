058746e4956a15bf12906b3e04687bd963930dbb
Kiki <horn_kristof@hotmail.com>
2018-06-18 18:24:52 +0200

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<hornkristof94>:<123456aA>@ds263460.mlab.com:63460/kiki' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
